5966d55268c30e2cf081001e8e279cab0dcc9dec
unknown <annisarihla29@gmail.com>
2026-05-05 14:47:40 +0800

    Create categories migration and model

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-Route::get('test', function () {
-return response()->json(['message' => 'OK']);
+
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'API berhasil',
+        'status' => 200
+    ]);
 });
